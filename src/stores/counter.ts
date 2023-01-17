0311d3264bfdf42dfc65cd5977317848bb5bf196
Gabriel Magreti <gabrielmagreti@gmail.com>
2023-01-17 18:25:07 -0300

    feat: add new counter

--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -2,12 +2,14 @@ import { create } from 'zustand';
 
 interface CounterState {
   count: number;
+  counTwo: number;
   increment: () => void;
-  decrement: () => void;
+  incrementTwo: () => void;
 }
 
 export const useCounter = create<CounterState>((set) => ({
   count: 0,
+  counTwo: 0,
   increment: () => set((state) => ({ count: state.count + 1 })),
-  decrement: () => set((state) => ({ count: state.count - 1 }))
+  incrementTwo: () => set((state) => ({ counTwo: state.counTwo + 1 })),
 }))
