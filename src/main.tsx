cbcde02bc12f546fb7ecd8469a25c6aedff6e233
Gabriel Magreti <gabrielmagreti@gmail.com>
2023-05-15 17:59:03 -0300

    feat: wrapping app with next ui provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-import { CounterProvider } from './context/CounterProvider'
-import { CounterUseSelectorProvider } from './context/CounterUseSelectorProvider'
+import { NextUIProvider } from "@nextui-org/react";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+import { CounterProvider } from "./context/CounterProvider";
+import { CounterUseSelectorProvider } from "./context/CounterUseSelectorProvider";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <CounterUseSelectorProvider>
-      <CounterProvider>
-        <App />
-      </CounterProvider>
-    </CounterUseSelectorProvider>
-  </React.StrictMode>,
-)
+    <NextUIProvider>
+      <CounterUseSelectorProvider>
+        <CounterProvider>
+          <App />
+        </CounterProvider>
+      </CounterUseSelectorProvider>
+    </NextUIProvider>
+  </React.StrictMode>
+);
